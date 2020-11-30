2288ec403e625682f0cdd5d9efa24df6ceee82b6
Jagdeep84 <71467410+Jagdeep84@users.noreply.github.com>
2020-12-01 05:01:54 +0530

    Add files via upload

---
+++ API_JagdeepSIngh/Controllers/RandomController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using API_JagdeepSIngh.Models;
+
+namespace API_JagdeepSIngh.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+            Random random = new Random();
+
+        [HttpGet]
+        public int Get()
+        {
+          
+            double Flt = random.NextDouble();
+            int Shift = Convert.ToInt32(Math.Floor(25 * Flt));
+            return Shift;
+        }
+        [HttpPost]
+        public Double Post()
+        {
+            double Flt = random.NextDouble();
+            return Flt;
+        }
+        [HttpPut]
+        public String Put()
+        {
+            return "Thank you for the new number!";
+        }
+        [HttpDelete]
+        public Char Delete() 
+        {
+
+         
+            double flt = random.NextDouble();
+            int shift = Convert.ToInt32(Math.Floor(25 * flt));
+            Char  Letter = Convert.ToChar(shift + 65);
+            return Letter;
+        }
+        [HttpPatch]
+        public DateTime Patich() 
+        {
+           var Date = DateTime.Now;
+            return Date;
+           
+        }
+        [HttpOptions]
+        public dynamic Options()
+        {
+            String Message = "Get: returns random Integer";
+            String Message2 = "Post: return random Double";
+            string Message3 = "Delete: return random Character";
+            string Message4 = "Patch: return Current Time";
+
+            var json = new { Message, Message2, Message3, Message4 };
+           return json;
+        }
+    }
+}
+
---
+++ API_JagdeepSIngh/Models/RandomClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_JagdeepSIngh.Models
+{
+    public class RandomClass
+    {
+        public Double Flt { get; set; }
+        public int Shift { get; set; }
+        public char Letter { get; set; }
+        public DateTime Date { get { return DateTime.Now; } }
+        public string Text { get; set; }
+        public string Message { get; set; }
+        public int code { get; set; }
+    }
+}
